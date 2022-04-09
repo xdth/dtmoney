1814957a4cd1890eb74e1dd9838be45a46ee1bae
xdth <info@dthlabs.com>
2022-04-09 19:10:25 -0400

    Integrating styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
+      <h1 className="title">Hello world</h1>
+      <GlobalStyle />
     </div>
   );
 }
